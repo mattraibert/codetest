a02cfffe1ba06d7ac886beaa190d25fce47085dc
Matt Raibert <mattraibert@gmail.com>
2014-04-16 11:50:15 -0400

    format outputs

--- src/PeopleParsing.hs
+++ src/PeopleParsing.hs
@@ -7,6 +7,9 @@ import Data.Text (Text)
 crunch :: Text -> Text
 crunch _ = ""
 
+format :: Text -> Text -> Text -> Text
+format one two three = T.concat ["Output 1:\n", one, "\nOutput 2:\n", two, "\nOutput 3:\n", three, "\n"]
+
 readFileText :: FilePath -> IO Text
 readFileText = (fmap T.pack) . readFile
 
--- src/Test.hs
+++ src/Test.hs
@@ -1,10 +1,14 @@
 {-# Language OverloadedStrings, FlexibleInstances, TypeFamilies, NoMonomorphismRestriction, ScopedTypeVariables, FlexibleContexts #-}
 module Test where
 
+import Data.Text (Text)
 import Test.Tasty
 import Test.Tasty.HUnit
 
-main :: IO ()
-main = defaultMain $ testGroup "tests" [ testCase "compare to model output" fooTest]
+import PeopleParsing
 
-fooTest = 1 @=? 1
+main :: IO ()
+main = defaultMain $ testGroup "tests" [ testCase "can format three outputs nicely" formatTest]
+
+formatTest :: Assertion
+formatTest = "Output 1:\n#{output1}\nOutput 2:\n#{output2}\nOutput 3:\n#{output3}\n" @=? format "#{output1}" "#{output2}" "#{output3}" 
