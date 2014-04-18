ae1ed9916ef47bfdfc96dea194b6144dfae6cd04
Matt Raibert <mattraibert@gmail.com>
2014-04-18 19:08:10 -0400

    Move fancy test cases into a new module

--- src/Test/Main.hs
+++ src/Test/Main.hs
@@ -7,6 +7,7 @@ import Data.List
 import Test.Tasty
 import Test.Tasty.HUnit
 
+import Test.Helper
 import Common
 
 import Person
@@ -37,15 +38,3 @@ jane = Person "Jane" "Jay" Female (Date 1979 4 2) "Green"
 winston = Person "Winston" "Watertown" Male (Date 1925 12 18) "Orange"
 wendy = Person "Wendy" "Watertown" Female (Date 1925 12 19) "Dark Orange"
 antonin = Person "Antonin" "Applebaum" Male (Date 1925 12 19) "Dark Gray"
-
-(.==.) :: (Show a, Eq a) => a -> a -> TestTree
-expected .==. actual = testCase ("expect " ++ (abbrev $ expected)) (expected @=? actual)
-
-(*==*) :: (Show a, Eq a) => [a] -> [a] -> TestTree
-expected *==* actual = testCase ("expect " ++ abbrevList expected) (expected @=? actual)
-
-abbrevList :: Show a => [a] -> String
-abbrevList = show . map (abbrev)
-
-abbrev :: (Show s) => s -> String
-abbrev t = take 10 (show t) ++ "..."
