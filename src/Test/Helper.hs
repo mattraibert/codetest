1d7863a1a718cee2c2bbece8ea57b847b3807292
Matt Raibert <mattraibert@gmail.com>
2014-04-19 12:50:56 -0400

    get rid of silly extra test operator

--- src/Test/Helper.hs
+++ src/Test/Helper.hs
@@ -4,13 +4,4 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 (.==.) :: (Show a, Eq a) => a -> a -> TestTree
-expected .==. actual = testCase ("expect " ++ (abbrev $ expected)) (expected @=? actual)
-
-(*==*) :: (Show a, Eq a) => [a] -> [a] -> TestTree
-expected *==* actual = testCase ("expect " ++ abbrevList expected) (expected @=? actual)
-
-abbrevList :: Show a => [a] -> String
-abbrevList = show . map (abbrev)
-
-abbrev :: (Show s) => s -> String
-abbrev t = take 10 (show t) ++ "..."
+expected .==. actual = testCase ("expect " ++ show expected) (expected @=? actual)
--- src/Test/Sort.hs
+++ src/Test/Sort.hs
@@ -9,14 +9,14 @@ import Person
 
 tests :: TestTree
 tests = testGroup "sorting" [
-  [Female, Male] *==* sort [Male, Female],
-  [Female, Female] *==* sort [Female, Female],
-  [jane, winston] *==* sortBy genderOrder [winston, jane],
-  [jane, winston] *==* sortBy lastNameOrder [winston, jane],
-  [winston, jane] *==* sortBy (flip lastNameOrder) [winston, jane],
-  [winston, wendy] *==* sortBy birthDateOrder [wendy, winston],
-  [jane, wendy, antonin] *==* sortBy genderThenLastNameOrder [antonin, jane, wendy],
-  [jane, wendy, antonin] *==* sortBy genderThenLastNameOrder [wendy, antonin, jane]]
+  [Female, Male] .==. sort [Male, Female],
+  [Female, Female] .==. sort [Female, Female],
+  [jane, winston] .==. sortBy genderOrder [winston, jane],
+  [jane, winston] .==. sortBy lastNameOrder [winston, jane],
+  [winston, jane] .==. sortBy (flip lastNameOrder) [winston, jane],
+  [winston, wendy] .==. sortBy birthDateOrder [wendy, winston],
+  [jane, wendy, antonin] .==. sortBy genderThenLastNameOrder [antonin, jane, wendy],
+  [jane, wendy, antonin] .==. sortBy genderThenLastNameOrder [wendy, antonin, jane]]
   where jane = Person "Jane" "Jay" Female (Date 1979 4 2) "Green"
         winston = Person "Winston" "Watertown" Male (Date 1925 12 18) "Orange"
         wendy = Person "Wendy" "Watertown" Female (Date 1925 12 19) "Dark Orange"
