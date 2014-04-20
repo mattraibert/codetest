c44b8090b036a069a55b11b7b91032e28fbcfcb5
Matt Raibert <mattraibert@gmail.com>
2014-04-20 09:15:14 -0400

    Use new hunit diff for more readable acceptance test output

--- src/Test/Helper.hs
+++ src/Test/Helper.hs
@@ -1,7 +1,14 @@
+{-# Language OverloadedStrings, FlexibleInstances, TypeFamilies, NoMonomorphismRestriction, ScopedTypeVariables, FlexibleContexts #-}
 module Test.Helper where
 
+import Data.Text (Text)
+import qualified Data.Text as T
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.HUnit.Diff
 
 (.==.) :: (Show a, Eq a) => a -> a -> TestTree
-expected .==. actual = testCase ("expect " ++ show expected) (expected @=? actual)
+expected .==. actual = testCase ("expect " ++ show expected) (expected @==? actual)
+
+($==$) :: Text -> Text -> TestTree
+expected $==$ actual = testCase "" (T.lines expected @==? T.lines actual)
